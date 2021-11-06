795dfca5e50e7767250c85e513dd57c6857fe2df
Ankit  Singh <76167572+anks1011@users.noreply.github.com>
2021-11-06 18:09:40 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,7 @@
+<!-- main footer start -->
+<div class="footer">
+          <p class="text-center">  2021 All rights reserved. Billing System. Developed by- <a
+                    href="https://www.linkedin.com/in/atifa-naaz-97b1641b3"> Atifa Naaz<a> and <a
+                              href="https://www.linkedin.com/in/anks1011/">Ankit Singh.<a>
+     
+</div>
